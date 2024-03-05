f363ba97cd94e1f704176a32e9019911d4d15074
Olena Pobehailo <106855523+OlenaPobehailo@users.noreply.github.com>
2024-03-05 15:41:50 +0200

    add pagination

--- backend/controllers/books.js
+++ backend/controllers/books.js
@@ -2,24 +2,38 @@ const { Book } = require("../models/book");
 // const { HttpError } = require("../helpers");
 const { ctrlWrapper } = require("../decorators");
 
-const getAll = async (req, res, next) => {
-  const result = await Book.find();
-  res.json(result);
+const getAll = async (req, res) => {
+  const { page = 1, limit = 12 } = req.query;
+  const skip = (page - 1) * limit;
+
+  const result = await Book.find().skip(skip).limit(limit);
+  res.json({ data: result });
 };
 
 const getBestsellers = async (req, res) => {
-  const bestsellers = await Book.find({ bestsellers: true });
-  res.json(bestsellers);
+  const { page = 1, limit = 12 } = req.query;
+  const skip = (page - 1) * limit;
+
+  const bestsellers = await Book.find({ bestsellers: true })
+    .skip(skip)
+    .limit(limit);
+  res.json({ data: bestsellers });
 };
 
 const getNewBooks = async (req, res) => {
-  const newBooks = await Book.find({ new: true });
-  res.json(newBooks);
+  const { page = 1, limit = 12 } = req.query;
+  const skip = (page - 1) * limit;
+  const newBooks = await Book.find({ new: true }).skip(skip).limit(limit);
+  res.json({ data: newBooks });
 };
 
 const getPromotions = async (req, res) => {
-  const promotions = await Book.find({ promotions: true });
-  res.json(promotions);
+  const { page = 1, limit = 12 } = req.query;
+  const skip = (page - 1) * limit;
+  const promotions = await Book.find({ promotions: true })
+    .skip(skip)
+    .limit(limit);
+  res.json({ data: promotions });
 };
 
 module.exports = {
