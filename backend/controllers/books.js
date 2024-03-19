3a21fbbf8b7c903c82bb2ee65357046008711581
MarynaOzkara <ozkaramaryna@gmail.com>
2024-03-19 12:46:10 +0200

    add filters books

--- backend/controllers/books.js
+++ backend/controllers/books.js
@@ -1,13 +1,16 @@
 const { Book } = require("../models/book");
 // const { HttpError } = require("../helpers");
 const { ctrlWrapper } = require("../decorators");
+const { query } = require("express");
+const { HttpError } = require("../helpers");
 
 const getAll = async (req, res) => {
-  const total = await Book.countDocuments();
   const { page = 1, limit = 12 } = req.query;
   const skip = (page - 1) * limit;
 
   const data = await Book.find().skip(skip).limit(limit);
+  const total = await Book.countDocuments();
+
   res.json({
     total,
     page,
@@ -48,7 +51,7 @@ const getPromotions = async (req, res) => {
   const total = await Book.countDocuments({ promotions: true });
   const { page = 1, limit = 12 } = req.query;
   const skip = (page - 1) * limit;
-  
+
   const data = await Book.find({ promotions: true }).skip(skip).limit(limit);
   res.json({
     total,
@@ -58,9 +61,92 @@ const getPromotions = async (req, res) => {
   });
 };
 
+const filtersBooks = async (req, res) => {
+  const {
+    promotions,
+    bestsellers,
+    title,
+    category,
+    subcategory,
+    language,
+    author,
+    publisher,
+    priceMin,
+    priceMax,
+    ratingMin,
+    ratingMax,
+    page = 1,
+    limit = 12,
+  } = req.query;
+  const skip = (page - 1) * limit;
+
+  const filters = {};
+  if (title) {
+    filters.title = { $regex: title, $options: "i" };
+  }
+  if (category) {
+    filters.category = { $regex: category, $options: "i" };
+  }
+  if (subcategory) {
+    filters.subcategory = { $regex: subcategory, $options: "i" };
+  }
+  if (language) {
+    filters.language = { $regex: language, $options: "i" };
+  }
+  if (author) {
+    filters.author = { $regex: author, $options: "i" };
+  }
+  if (publisher) {
+    filters.publisher = { $regex: publisher, $options: "i" };
+  }
+  if (promotions) {
+    filters.promotions = promotions;
+  }
+  if (bestsellers) {
+    filters.bestsellers = bestsellers;
+  }
+  if (req.query.new) {
+    filters.new = req.query.new;
+  }
+  if (priceMin || priceMax) {
+    filters.price = {};
+    if (priceMin) {
+      filters.price.$gte = +priceMin;
+    }
+    if (priceMax) {
+      filters.price.$lte = +priceMax;
+    }
+  }
+  if (ratingMin || ratingMax) {
+    filters.rating = {};
+    if (ratingMin) {
+      filters.rating.$gte = +ratingMin;
+    }
+    if (ratingMax) {
+      filters.rating.$lte = +ratingMax;
+    }
+  }
+
+  const books = await Book.find(filters).skip(skip).limit(limit);
+  const total = await Book.countDocuments(filters);
+  if (!books) {
+    throw HttpError(404, "Books not found");
+  }
+  if (total === 0) {
+    res.status(404).json({ massage: "Books not found" });
+  }
+  res.status(200).json({
+    total,
+    page,
+    limit,
+    books,
+  });
+};
+
 module.exports = {
   getAll: ctrlWrapper(getAll),
   getBestsellers: ctrlWrapper(getBestsellers),
   getNewBooks: ctrlWrapper(getNewBooks),
   getPromotions: ctrlWrapper(getPromotions),
+  filtersBooks: ctrlWrapper(filtersBooks),
 };
--- backend/routes/api/books.js
+++ backend/routes/api/books.js
@@ -8,5 +8,6 @@ router.get("/", booksController.getAll);
 router.get("/bestsellers", booksController.getBestsellers);
 router.get("/new", booksController.getNewBooks);
 router.get("/promotions", booksController.getPromotions);
+router.get("/filters", booksController.filtersBooks);
 
 module.exports = router;
