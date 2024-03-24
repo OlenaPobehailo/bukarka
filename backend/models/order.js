b32777657507db523512c7e0233aa4bc25db7791
Olena Pobehailo <106855523+OlenaPobehailo@users.noreply.github.com>
2024-03-24 13:04:24 +0200

    add getOrderById, placeOrder

--- backend/models/order.js
+++ backend/models/order.js
@@ -22,6 +22,48 @@ const orderSchema = new Schema(
       default: 0,
     },
 
+    isGuestOrder: {
+      type: Boolean,
+      default: false,
+    },
+
+    customerInfo: {
+      name: {
+        type: String,
+        required: function () {
+          return this.isModified("customerInfo");
+        },
+      },
+      surname: {
+        type: String,
+        required: function () {
+          return this.isModified("customerInfo");
+        },
+      },
+      email: {
+        type: String,
+        required: function () {
+          return this.isModified("customerInfo");
+        },
+      },
+      phoneNumber: {
+        type: String,
+        required: function () {
+          return this.isModified("customerInfo");
+        },
+      },
+      address: {
+        type: String,
+        required: function () {
+          return this.isModified("customerInfo");
+        },
+      },
+      comment: {
+        type: String,
+        // default: "",
+      },
+    },
+
     // user: {
     //     type: Schema.Types.ObjectId,
     //     ref: 'User',
