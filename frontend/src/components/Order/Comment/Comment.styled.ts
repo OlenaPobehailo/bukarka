94c91f7325a582f7abc9708c62189abdcda7c6ba
Olena Pobehailo <106855523+OlenaPobehailo@users.noreply.github.com>
2024-03-28 07:40:27 +0200

    add Comment markup

--- frontend/src/components/Order/Comment/Comment.styled.ts
+++ frontend/src/components/Order/Comment/Comment.styled.ts
@@ -1,3 +1,14 @@
 import styled from "styled-components";
 
-export const a = styled.div``;
+export const Textarea = styled.textarea`
+  width: 100%;
+  height: 80px;
+  padding: 16px;
+  border: 1px solid var(--bukarka-light-grey-1);
+  resize: vertical;
+
+  font-family: var(--regular);
+  font-size: 16px;
+  line-height: 1.5;
+  color: var(--bukarka-dark-grey);
+`;
--- frontend/src/components/Order/Comment/Comment.tsx
+++ frontend/src/components/Order/Comment/Comment.tsx
@@ -1,10 +1,24 @@
+import { useState } from "react";
+import { Label, SubTitle, Wrapper } from "../OrderCommonStyled";
+import { Textarea } from "./Comment.styled";
 
-const Comment :React.FC= () => {
+const Comment: React.FC = () => {
+  const [inputValue, setInputValue] = useState("");
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setInputValue(e.target.value);
+  };
+
   return (
-    <div>
-      Comment
-    </div>
-  )
-}
+    <Wrapper>
+      <SubTitle>Коментар до замовлення</SubTitle>
+      <Textarea
+        value={inputValue}
+        onChange={handleInputChange}
+        placeholder="Залиште свій коментар (опціонально)"
+      />
+    </Wrapper>
+  );
+};
 
-export default Comment
+export default Comment;
