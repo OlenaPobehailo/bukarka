18f33545f1c32a767ed8628d3d177921891ddf4f
tanyushanel <tatyana.nelipovich@gmail.com>
2024-05-24 19:25:48 +0200

    feat: del unused console.logs

--- frontend/src/components/Book/BooksContext.tsx
+++ frontend/src/components/Book/BooksContext.tsx
@@ -6,12 +6,7 @@ import React, {
   useState,
 } from "react";
 import { instance } from "utils/fetchInstance";
-import {
-  BooksContextType,
-  IBookItem,
-  IBooksData,
-  IBooksResponseData,
-} from "./Book.types";
+import { BooksContextType, IBookItem, IBooksData } from "./Book.types";
 
 const BooksContext = createContext<BooksContextType | null>(null);
 
@@ -23,11 +18,6 @@ export const useBooks = (): BooksContextType => {
   return context;
 };
 
-// const fetchBooks = async (): Promise<IBooksResponseData> => {
-//   const response = await instance.get("/api/books");
-//   return response;
-// };
-
 const fetchBooks = async (): Promise<IBookItem[]> => {
   let books: IBookItem[] = [];
   let page = 1;
@@ -38,10 +28,7 @@ const fetchBooks = async (): Promise<IBookItem[]> => {
       params: { page },
     });
 
-    console.log(response);
     books = books.concat(response.data.data);
-    console.log(books);
-    console.log(response.data.data);
 
     totalPages =
       response.data.total && response.data.limit
@@ -49,8 +36,6 @@ const fetchBooks = async (): Promise<IBookItem[]> => {
         : 1;
     page += 1;
   } while (page <= totalPages);
-
-  console.log(books);
 
   return books;
 };
@@ -61,13 +46,11 @@ export const BooksContextProvider: React.FC<{ children: ReactNode }> = ({
   const [booksData, setBooksData] = useState<IBookItem[]>([]);
   const [favorites, setFavorites] = useState<string[]>([]);
 
-  console.log(booksData);
-
   useEffect(() => {
     const loadBooks = async () => {
       try {
         const allBooks = await fetchBooks();
-        console.log(allBooks);
+
         setBooksData(allBooks);
       } catch (error) {
         console.error("Error fetching books:", error);
--- frontend/src/components/Favorites/Favorites.tsx
+++ frontend/src/components/Favorites/Favorites.tsx
@@ -1,9 +1,8 @@
-import { IBookItem, IBooksData } from "components/Book/Book.types";
+import { IBooksData } from "components/Book/Book.types";
 import CartItem from "components/Home/CartItem/CartItem";
 import { FlexWrapper, Label, Wrapper } from "pages/CommonPages.styled";
 
 const Favorites: React.FC<IBooksData> = ({ data }) => {
-  console.log(data);
   return (
     <Wrapper>
       <Label>Обране</Label>
